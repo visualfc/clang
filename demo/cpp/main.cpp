7d743226135703701af5d67607b68e2251e23d52
visualfc <visualfc@gmail.com>
2025-07-23 20:42:57 +0800

    x

--- demo/cpp/main.cpp
+++ demo/cpp/main.cpp
@@ -2,31 +2,34 @@
 #include <stdio.h>
 
 // 定义回调函数
+extern "C"
 CXChildVisitResult visitChildrenCallback(CXCursor cursor, 
                                        CXCursor parent, 
                                        CXClientData client_data) {
     // 获取光标的位置和名称
-    CXString cursorKindName = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
-    CXString cursorSpelling = clang_getCursorSpelling(cursor);
-    CXSourceLocation location = clang_getCursorLocation(cursor);
-    
-    CXString filename;
-    unsigned line, column;
-    clang_getPresumedLocation(location, &filename, &line, &column);
-    
-    printf("Found %s '%s' at %s:%d:%d\n", 
-           clang_getCString(cursorKindName),
-           clang_getCString(cursorSpelling),
-           clang_getCString(filename),
-           line, column);
-    
-    // 释放资源
-    clang_disposeString(cursorKindName);
-    clang_disposeString(cursorSpelling);
-    clang_disposeString(filename);
-    
-    // 继续遍历子节点
+    printf("visit\n");
     return CXChildVisit_Recurse;
+//    CXString cursorKindName = clang_getCursorKindSpelling(clang_getCursorKind(cursor));
+//    CXString cursorSpelling = clang_getCursorSpelling(cursor);
+//    CXSourceLocation location = clang_getCursorLocation(cursor);
+    
+//    CXString filename;
+//    unsigned line, column;
+//    clang_getPresumedLocation(location, &filename, &line, &column);
+    
+//    printf("Found %s '%s' at %s:%d:%d\n", 
+//           clang_getCString(cursorKindName),
+//           clang_getCString(cursorSpelling),
+//           clang_getCString(filename),
+//           line, column);
+    
+//    // 释放资源
+//    clang_disposeString(cursorKindName);
+//    clang_disposeString(cursorSpelling);
+//    clang_disposeString(filename);
+    
+//    // 继续遍历子节点
+//    return CXChildVisit_Recurse;
 }
 
 int main(int argc, char *argv[]) {
